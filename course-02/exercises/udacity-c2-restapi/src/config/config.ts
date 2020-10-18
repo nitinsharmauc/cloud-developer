e2a8aa593a791b801a0914b481ad429a44261fac
unknown <nitinsharmauc@gmail.com>
2020-10-18 16:12:41 +0530

    Add DEPLOYED env variable for EB deployment

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -2,12 +2,12 @@ export const config = {
   "dev": {
     "username": process.env.POSTGRES_USERNAME,
     "password": process.env.POSTGRES_PASSWORD,
-    "database": "udagramnsdev",
-    "host": "udagramnsdev.cqyijfdwjyxe.ap-southeast-1.rds.amazonaws.com",
+    "database": process.env.POSTGRES_DATABASE,
+    "host": process.env.POSTGRES_HOST,
     "dialect": "postgres",
     "aws_region": process.env.AWS_REGION,
     "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": "udagram-ns-dev"
+    "aws_media_bucket": process.env.AWS_MEDIA_BUCKET
   },
   "prod": {
     "username": "",
@@ -17,6 +17,6 @@ export const config = {
     "dialect": "postgres"
   },
   "jwt": {
-    "secret": "helloworld"
+    "secret": process.env.JWT_SECRET
   }
 }
